bac63c0c201006426c54f036064c4d812058265f
Vlad <peckovychv@gmail.com>
2024-10-03 18:42:17 +0300

    new inage on a home page

--- src/app/components/home/home.jsx
+++ src/app/components/home/home.jsx
@@ -4,6 +4,8 @@ import useFetchVehicleMakes from "../../hooks/useFetchVehicleMakes";
 import VehicleMakesDropdown from "./VehicleMakesDropdown";
 import YearsDropdown from "./YearsDropdown";
 import NextButton from "../buttons/nextButton";
+import carLogo from '../../../../public/carLogo.png'
+import Image from "next/image";
 
 function Home() {
   const { data, loading } = useFetchVehicleMakes();
@@ -12,7 +14,8 @@ function Home() {
   return (
     <div className="h-screen bg-black text-white flex flex-col">
       <div>
-        <h1 className="text-2xl m-5">Vehicle Makes</h1>
+        <h1 className="text-2xl m-5">Find your dream car</h1>
+        <Image className="h-[200px] object-contain m-10  ml-0" src={carLogo} alt={''}></Image>
         <div className="flex flex-row space-x-4">
           {loading && <p>Loading ...</p>}
           {!loading && data && (
@@ -25,6 +28,7 @@ function Home() {
       <div className="mt-6">
         <NextButton makeId={selectedMake} year={selectedYear} />
       </div>
+      
     </div>
   );
 }
